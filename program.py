4272146b637bbba9265ebfcb1b0bc5cca7d203b5
Dorkom <varerik95@gmail.com>
2022-12-27 22:49:24 -0500

    por hacer

--- program.py
+++ program.py
@@ -8,9 +8,7 @@ import tkinter.font
 db = Database('maq.db')
 
 mainwin=Tk()
-# mainwin.geometry('1500x300')
 mainwin.maxsize= (1500, 300)
-# mainwin.maxsize= (50000, 500)
 
 my_font = tkinter.font.Font(mainwin,family="Arial Black", size=12)
 my_fonts = tkinter.font.Font(mainwin,family="Arial Black", size=10)
@@ -32,11 +30,7 @@ global flag_ingresar_excavadora
 flag_ingresar_excavadora = 0
 print(thisid)
 
-frame_main = tkinter.Frame(mainwin)
-frame_main.grid(sticky='news')
-
 def small_populate_list():
-    frame_canvas = tkinter.Frame(frame_main)
     marco1_label = Label(mainwin, text='Excavadora', font=my_fontdisplay)
     marco1_label.grid(row=1, column=1, sticky=W)
     marco1_label = Label(mainwin, text='Cuchara', font=my_fontdisplay)
@@ -46,45 +40,31 @@ def small_populate_list():
     marco1_label = Label(mainwin, text='Rendimiento', font=my_fontdisplay)
     marco1_label.grid(row=1, column=4, sticky=W)
     i = 2
-    # vari = 0
     for row in db.smallfetch():
-        # parts_list.insert(END, row)
         try:
             n = 0
-            # vari = vari+1
             print('populate list')
             print(thisid)
-            # print('vari')
-            # print(vari)
-            # if vari == thisid:
-            #     break
             for j in range(len(row)):
                 e = Text(mainwin, width=16, height=2, fg='black', font=my_fontdisplay, wrap=None)
                 e.grid(row=i, column=j)
-                # e.insert(END, row[j])
-                if n == 0:
-                    e.config(width=4)
-                    e.insert(END, row[j])
-                elif n == 1 or n == 2:
-                    e.config(width=13)
-                    e.insert(END, row[j])
-                else:
-                    e.config(width=10)
-                    e.insert(END, round(float(str(row[j])), 2))
+                # if n == 0:
+                #     e.config(width=4)
+                #     e.insert(END, row[j])
+                # elif n == 1 or n == 2:
+                #     e.config(width=13)
+                #     e.insert(END, row[j])
+                # else:
+                #     e.config(width=10)
+                #     e.insert(END, round(float(str(row[j])), 2))
 
-                # e.insert(END, row[j])
+                e.insert(END, row[j])
                 n = n + 1
             i=i+1
-            # vacio_label = Label(mainwin, text='', pady=10)
-            # vacio_label.grid(row=i, column=0)
-            
-            # vsb = Scrollbar(mainwin, orient='vertical', command=mainwin.yview)
-            # vsb.grid(row=i, column=j+2)
         except IndexError:
             pass
 
 def populate_list():
-    # parts_list.delete(0, END)
     # excavadora, excavacion, balanceo, carga, tiempocarga, cuchara, eficiencia, capacidadcuchara, capacidadneta, alto, ancho, largo, volumen, sobreexcavacion, tiempoalquiler, numerocicloshora, productividad, rendimiento
     marco1_label = Label(mainwin, text='Excavadora', font=my_fontdisplay)
     marco1_label.grid(row=1, column=1, sticky=W)
@@ -123,42 +103,37 @@ def populate_list():
     marco1_label = Label(mainwin, text='Rendimiento', font=my_fontdisplay)
     marco1_label.grid(row=1, column=18, sticky=W)
     i = 2
-    # vari = 0
     for row in db.fetch():
-        # parts_list.insert(END, row)
         try:
             n = 0
-            # vari = vari+1
             print('populate list')
             print(thisid)
-            # print('vari')
-            # print(vari)
-            # if vari == thisid:
-            #     break
             for j in range(len(row)):
                 e = Text(mainwin, width=16, height=2, fg='black', font=my_fontdisplay, wrap=None)
                 e.grid(row=i, column=j)
-                # e.insert(END, row[j])
                 if n == 0 or n == 1 or n == 6:
                     e.insert(END, row[j])
                 else:
                     e.insert(END, round(float(str(row[j])), 2))
-                    # e.insert(END, row[j])
+                # e.insert(END, row[j])
                 n = n + 1
             i=i+1
             vacio_label = Label(mainwin, text='', font=my_font, pady=10, padx=10)
             vacio_label.grid(row=i, column=0)
-            
-            # vsb = Scrollbar(mainwin, orient='vertical', command=mainwin.yview)
-            # vsb.grid(row=i, column=j+2)
         except IndexError:
             pass
 
+def actualizar_lista():
+    global thisid
+    if flag1 != 0:
+        thisid = 0
+        for row in db.fetch():
+            thisid = thisid + 1
+        small_populate_list()
 
 def escoger_excavadora():
     mainwin.withdraw()
     selwin=Toplevel()
-    # selwin.geometry('330x800')
     selwin.maxsize(330, 800)
 
     def calcular_datos():
@@ -188,24 +163,12 @@ def escoger_excavadora():
                     sobreexcava_text.set(round(sobreexcava, 2))
                     tiempoalquiler_text.set(round(tiempoalquiler, 2))
                     numerocicloshora_text.set(round(numerocicloshora, 2))
-                    # capacidadhora_text.set(round(capacidadporhora, 2))
                     productividad_text.set(round(productividad, 2))
                     rendimiento_text.set(round(rendimiento, 2))
                     
                     flag_ingresar_excavadora = 0
                     global flag1
                     flag1 = 1
-
-                    # if tiempo1_text.get() != '' or tiempo2_text.get() != '' or tiempo3_text.get() != '':
-                    #     calt = float(tiempo1_text.get()) + float(tiempo2_text.get()) + float(tiempo3_text.get())
-                    #     prod = float(calt)*float(volumen_text.get())
-                    #     rend = prod*8.0
-                    #     productividad_text.set(prod)
-                    #     rendimiento_text.set(rend)
-                    # print('productividad')
-                    # print(prod)
-                    # print('rendimiento')
-                    # print(rend)
                 except ValueError:
                     flag_ingresar_excavadora = 1
                     messagebox.showerror('Not a number', 'Please insert a number')
@@ -301,19 +264,12 @@ def escoger_excavadora():
         tiempoalquiler_label2 = Label(app, text='h', font=my_font, pady=10, padx=10)
         tiempoalquiler_label2.grid(row=11, column=2, sticky=W)
 
-        # capacidadhora_text = StringVar(app,'','capacidadhora')
-        # capacidadhora_label = Label(app, text='Capacidad por hora', font=my_font, pady=10, padx=10)
-        # capacidadhora_label.grid(row=8, column=0, sticky=W)
-        # capacidadhora_output = Label(app, textvariable=capacidadhora_text, relief=RAISED, font=my_font)
-        # capacidadhora_output.grid(row=8, column=1)
-
         # Buttons
         calcular_btn = Button(app, text='Calcular rendimiento', width=20, command=calcular_rend, font=my_fonts)
         calcular_btn.grid(row=12, column=0, pady=10, padx=20)
 
         app.title('Rendimiento')
         app.maxsize(1000, 1000)
-        # app.geometry('400x600')
 
         app.protocol("WM_DELETE_WINDOW", on_closingapp)
         app.mainloop()
@@ -325,15 +281,12 @@ def escoger_excavadora():
         if 'flag_ingresar_excavadora' in globals():
             if flag_ingresar_excavadora == 1:
                 db.remove(thisid)
-                # db.remove(thisid+1)
                 flag_ingresar_excavadora = 0
         actualizar_lista()
         mainwin.deiconify()
         selwin.destroy()
 
     def comboclick1(event):
-        # imagenexca = Label(selwin, text=myCombo1.get(), font=my_font, pady=10, padx=10)
-        # imagehy = PhotoImage(file='Hyundai_Robex_200LC-9SB.jpg')
         if myCombo1.get() == "Hyundai Robex 200LC-9SB":
             imagemaq = Image.open('Hyundai_Robex_200LC-9SB.jpg')
             imagemaqr = imagemaq.resize((150,150))
@@ -429,8 +382,6 @@ def escoger_excavadora():
             flag1 = 0
 
             def ingresaexca():
-                # global flag1
-                # flag1 = 0
                 global flag_ingresar_excavadora
                 global thisid
                 if not myCombo1.get() or not myCombo2.get() or excavacion_text.get() == '' or balanceo_text.get() == '' or carga_text.get() == '':
@@ -481,11 +432,6 @@ def escoger_excavadora():
     selcu_label = Label(selwin, text='Seleccionar cucharon:', font=my_fontb, pady=10, padx=10)
     selcu_label.grid(row=3, column=0, sticky=W)
 
-    # myCombo2 = ttk.Combobox(selwin, value=cuchoptions, width=25)
-    # myCombo2.current()
-    # myCombo2.bind("<<ComboboxSelected>>", comboclick2)
-    # myCombo2.grid(row=4, column=0, sticky=W, pady=10, padx=10)
-
     ingresardat_label = Label(selwin, text='Ingresar tiempo de carga:', font=my_fontb, pady=10, padx=10)
     ingresardat_label.grid(row=6, column=0, sticky=W)
 
@@ -530,12 +476,21 @@ def escoger_excavadora():
     selwin.configure(bg='white')
     selwin.mainloop()
 
+def eliminar_id():
+    db.remove(eliminar_text.get())
+    mainwin.update()
+    actualizar_lista()
 
-
-iniciar_btn = Button(mainwin, text='Iniciar', width=10, command=escoger_excavadora, font=my_fonts)
+iniciar_btn = Button(mainwin, text='Iniciar', width=6, command=escoger_excavadora, font=my_fonts)
 iniciar_btn.grid(row=0, column=0, pady=10, padx=10)
-detalles_btn = Button(mainwin, text='Detalles', width=10, command=populate_list, font=my_fonts)
+detalles_btn = Button(mainwin, text='Detalles', width=7, command=populate_list, font=my_fonts)
 detalles_btn.grid(row=0, column=1, pady=10, padx=10)
+
+eliminar_btn = Button(mainwin, text='Eliminar', width=7, command=eliminar_id, font=my_fonts)
+eliminar_btn.grid(row=0, column=2, pady=10, padx=10)
+eliminar_text = StringVar(mainwin,'','eliminar')
+eliminar_entry = Entry(mainwin, textvariable=eliminar_text, width=4, font=my_font)
+eliminar_entry.grid(row=0, column=3)
 
 def actualizar_lista():
     global thisid
@@ -545,30 +500,6 @@ def actualizar_lista():
             thisid = thisid + 1
         small_populate_list()
 
-# actualizar_btn = Button(mainwin, text='Actualizar lista', width=20, command=actualizar_lista)
-# actualizar_btn.grid(row=0, column=1, pady=10, padx=10)
-
-# parts_list = Listbox(mainwin, height=10, width=180, border=1)
-# parts_list.grid(row=1, column=0, columnspan=19, rowspan=6, pady=20, padx=20)
-
-# scrollbar = Scrollbar(mainwin)
-# scrollbar.grid(row=1, column=3)
-
-# parts_list.configure(yscrollcommand=scrollbar.set)
-# scrollbar.configure(command=parts_list.yview)
-
 small_populate_list()
 
 mainwin.mainloop()
-
-# r_set=my_conn.execute('''SELECT * from student LIMIT 0,10''');
-# i=0 # row value inside the loop 
-# for student in r_set: 
-#     for j in range(len(student)):
-#         e = Entry(my_w, width=10, fg='blue') 
-#         e.grid(row=i, column=j) 
-#         e.insert(END, student[j])
-#     i=i+1
-# my_w.mainloop()
-
-# pyinstaller program.py --onefile --windowed --add-data "Hyundai_Robex_200LC-9SB.jpg;." --add-data "Doosan_DX225LCA.jpg;." --add-data "SAE_APILADO_N_01.jpg;." --add-data "SAE_APILADO_N_02.jpg;." --add-data "SAE_APILADO_N_03.jpg;." --add-data "SAE_APILADO_N_04.jpg;." --add-data "SAE_APILADO_N_05.jpg;." --add-data "SAE_APILADO_N_06.jpg;." --add-data "SAE_APILADO_N_07.jpg;." --add-data "SAE_APILADO_N_08.jpg;." --add-data "cucharon_de_uso_general.jpg;." --add-data "cucharon_de_servicio_pesado.jpg;." --add-data "cucharon_de_servicio_severo.jpg;." --add-data "maq.db;."
